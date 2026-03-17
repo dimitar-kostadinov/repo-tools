0c7079283d86475d652cb33cc5fdd086c1c33b4b
I024114 <dimitar.kostadinov@sap.com>
2026-03-17 15:48:11 +0200

    Fix logcheck module

--- hack/tools/logcheck/main.go
+++ hack/tools/logcheck/main.go
@@ -7,7 +7,7 @@ package main
 import (
 	"golang.org/x/tools/go/analysis/singlechecker"
 
-	"github.com/gardener/gardener/hack/tools/logcheck/pkg/logcheck"
+	"github.com/dimitar-kostadinov/repo-tools/hack/tools/logcheck/pkg/logcheck"
 )
 
 func main() {
